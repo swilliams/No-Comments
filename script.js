24ef1585ab602a87d3f4bf4eb5685d0ce6a343df
Scott Williams <scott@krazyyak.com>
2012-03-21 08:58:03 -0700

    Updated plugin to be content script. Added script to remove comment links and comment table.

---
+++ script.js
@@ -0,0 +1,28 @@
+function removeIndexComments() {
+
+	var rows = document.getElementsByClassName('subtext');
+	for (var i = 0; i < rows.length; i+=1) {
+		var row = rows[i];
+		var tags = row.getElementsByTagName('a');
+		for (var j = 0; j < tags.length; j++) {
+	        var text = tags[j].innerHTML;
+
+		    if (text.search('comment') > 0 || text.search('discuss') >= 0) {
+		    	row.removeChild(tags[j]);
+	        }
+		}
+	}
+}
+
+function removeComments() {
+	var tables = document.getElementsByTagName('table'),
+	    comments = tables[3];
+	comments.innerHTML = '';
+}
+
+var url = location.href;
+if (url.search('item') > 0) {
+	removeComments();
+} else {
+	removeIndexComments();
+}
